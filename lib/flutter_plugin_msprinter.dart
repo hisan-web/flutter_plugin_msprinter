3a855f014ca47410e451445f1fc573fbedab5ab9
dev@hisan-web.com <>
2021-01-30 11:34:07 +0800

    初始化

---
+++ lib/flutter_plugin_msprinter.dart
@@ -0,0 +1,14 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterPluginMsprinter {
+  static const MethodChannel _channel =
+      const MethodChannel('flutter_plugin_msprinter');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
